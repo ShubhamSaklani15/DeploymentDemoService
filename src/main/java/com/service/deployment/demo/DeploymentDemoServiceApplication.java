6c9f063ccb990cbc2ccd40c07c9d6d596d1b5a98
Shubham Saklani <saklanishubham15@gmail.com>
2024-09-05 00:16:16 +0530

    Initial commit

---
+++ src/main/java/com/service/deployment/demo/DeploymentDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.deployment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentDemoServiceApplication.class, args);
+	}
+
+}
